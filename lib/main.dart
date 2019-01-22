ac76d21ba27094c6b2dab8bfa1a626966d8b09f4
孙攀 <sunpan920@gmail.com>
2019-01-22 11:42:35 +0800

    使用StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
@@ -9,12 +10,27 @@ class MyApp extends StatelessWidget {
       title: 'Welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
-          title: const Text('Welocme to Flutter'),
+          title: new Text('Welocme to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: new Center(
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPairs = new WordPair.random();
+    return new Text(wordPairs.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RandomWordsState();
   }
 }
